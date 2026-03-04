9e58ec75e7315b85a1d5372d6c0528dce90f28ea
Shelly Fourer <shellyfourer@gmail.com>
2026-03-04 19:10:19 +0100

    Add layered architecture packages (controller/service/repository)

---
+++ backend/src/main/java/com/shelly/freelancetaxmanager/repository/TaxRepository.java
@@ -0,0 +1,8 @@
+package com.shelly.freelancetaxmanager.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TaxRepository {
+    // TODO: later connect to database (PostgreSQL)
+}
---
+++ backend/src/main/java/com/shelly/freelancetaxmanager/service/TaxService.java
@@ -0,0 +1,8 @@
+package com.shelly.freelancetaxmanager.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaxService {
+    // TODO: implement Lithuanian tax calculation
+}
